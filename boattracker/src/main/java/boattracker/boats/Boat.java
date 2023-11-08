1170ffd0b3adadc41f50c6bb20a89724101a7466
ethoprak <93220679+ethoprak@users.noreply.github.com>
2023-11-08 17:03:45 -0500

    “Separated into internal and boats packages”

---
+++ boattracker/src/main/java/boattracker/boats/Boat.java
@@ -0,0 +1,30 @@
+package boattracker;
+import java.util.ArrayList;
+
+public class Boat {
+    private String modelNumber;
+    private String boatColor;
+    private double boatPrice = 0.0;
+    private ArrayList<Employee> users = new ArrayList<Employee>();
+
+    public Boat(String modelNumber){
+        this.modelNumber = modelNumber;
+    }
+
+    public String getBoatColor() {
+        return boatColor;
+    }
+ 
+    public double getBoatPrice() {
+        return boatPrice;
+    }
+
+    public void addUser(Employee employee){
+	this.users.add(employee);
+    }
+
+    public void removeUser(Employee employee){
+	this.users.remove(employee);
+    }
+
+}
